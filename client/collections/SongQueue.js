b3e20bd5a28eb6c7855b7cb58c1514ae9a0f84b2
kwong3672 <keith.wong3672@gmail.com>
2016-06-22 21:08:11 -0700

    Add some handlers for SongQueue & pass specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,24 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.length < 2) {
+        this.playFirst();
+      }
+    });
 
+    this.on('ended', function () {
+      this.shift();
+      if (this.length > 0) { this.playFirst(); }
+    });
+
+    this.on('dequeue', function() {
+      this.remove();
+    });
+  },
+
+  playFirst: function() {
+    var song = this.at(0);
+    if (song) { song.play(); }
+  }
 });
