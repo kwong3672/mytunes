c24f019cd860acb599758d5e88d1d1ebefdc5e39
kwong3672 <keith.wong3672@gmail.com>
2016-06-23 12:40:54 -0700

    Pass Songs collection specs

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,17 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
-
+  model: SongModel,
+  url: '/https://api.parse.com/1/classes/songs',
+  initialize: function() {
+    var self = this;
+    var options = {
+      success: function(data) {
+        data.results.forEach(function(song) {
+          self.add(song);
+        });
+      }
+    };
+    Backbone.sync('read', this, options);
+  }
 });
