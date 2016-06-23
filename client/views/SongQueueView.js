7a312f16b6a3bd9fd1de5c01701b6f400d0a0241
kwong3672 <keith.wong3672@gmail.com>
2016-06-22 21:29:08 -0700

    Pass tests for SongQueueView

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,11 +2,21 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    this.collection.on('remove', function() {
+      this.render();
+    }, this);
+    this.collection.on('add', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
-  },
-  initialize: function() {
+    var collection = this.collection;
+
+    _.each(collection, function(song) {
+      var view = new SongQueueEntryView(song);
+      view.render();
+    });
   }
 
 });
